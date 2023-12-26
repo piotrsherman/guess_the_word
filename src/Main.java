e0aff845e39518dbc817d783779b99c4a0511439
piotrsherman <piotr.s.herman@gmail.com>
2023-12-26 19:20:11 +0100

    logic refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,68 @@
 import java.util.Scanner;
-
 public class Main {
-    private static final String word = "moloko";
-
+    private static final int MAX_TRIES = 7;
+    private static final char BLANK_CHAR = '-';
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Guess the word");
-        int wordLength = word.length();
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < wordLength; i++) {
-            stringBuilder.append("-");
+        Scanner scanner = new Scanner(System.in);
+        int wrongAttempts = 0;
+        String wordToGuess = "hello";
+        char[] guessedLetters = new char[wordToGuess.length()];
+        for (int i = 0; i < guessedLetters.length; i++) {
+            guessedLetters[i] = BLANK_CHAR;
         }
-        String maskWord = stringBuilder.toString();
-        int incorrectGuesses = 0; // initialize counter for incorrect guesses
-        int now = 0;
-        Gallows gallows = new Gallows();
-
-        System.out.println(maskWord);
-
-        while (incorrectGuesses < 7 && maskWord.contains("-")) { // continue the game until 7 incorrect guesses or word is guessed
-            System.out.print("Enter a letter: ");
-
-            char c = input.next().charAt(0);
-            if (word.toLowerCase().indexOf(c) >= 0) {
-                System.out.println("You are right");
-                for (int i = 0; i < word.length(); i++) {
-                    if (word.charAt(i) == c) {
-                        maskWord = replaceLetter(Character.toString(c), maskWord);
-                    }
-                }
-                System.out.println(maskWord);
-            } else {
-                System.out.println("No, try again");
-                incorrectGuesses++; // increment the counter for incorrect guesses
-                now = 7 - incorrectGuesses;
-                System.out.println("You have " + (now) + " incorrect guesses left"); // display the number of incorrect guesses left
-                if (now == 6) { gallows.sixErrorsLeft();}
-                if (now == 5) { gallows.fiveErrorsLeft();}
-                if (now == 4) { gallows.foreErrorsLeft();}
-                if (now == 3) { gallows.threeErrorsLeft();}
-                if (now == 2) { gallows.twoErrorsLeft();}
-                if (now == 1) { gallows.oneErrorsLeft();}
-                if (now == 0) { gallows.zeroErrorsLeft();}
+        while (wrongAttempts < MAX_TRIES && new String(guessedLetters).contains("-")) {
+            System.out.println("Guess a letter: ");
+            char guess = scanner.next().charAt(0);
+            if (!Character.isLetter(guess)) {
+                System.out.println("Please enter a valid letter.");
+                continue;
             }
+            boolean found = false;
+            for (int i = 0; i < wordToGuess.length(); i++) {
+                if (wordToGuess.charAt(i) == guess) {
+                    guessedLetters[i] = guess;
+                    found = true;
+                }
+            }
+            if (!found) {
+                wrongAttempts++;
+                System.out.println("Wrong guess! Attempts left: " + (MAX_TRIES - wrongAttempts));
+            }
+            drawHangman(wrongAttempts);
+            System.out.println("Current progress: " + new String(guessedLetters));
         }
-
-        if (!maskWord.contains("-")) {
-            System.out.println("Congratulations, you won!");
+        if (!new String(guessedLetters).contains("-")) {
+            System.out.println("Congratulations! You guessed the word: " + wordToGuess);
         } else {
-            System.out.println("Sorry, you lost. The word was " + word);
+            System.out.println("Sorry, you ran out of attempts. The word was: " + wordToGuess);
         }
     }
-
-    private static String replaceLetter(String letter, String maskWord) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == letter.charAt(0)) {
-                stringBuilder.append(letter);
-            } else if (maskWord.charAt(i) != '-') {
-                stringBuilder.append(maskWord.charAt(i));
-            } else {
-                stringBuilder.append("-");
-            }
+    private static void drawHangman(int wrongAttempts) {
+        Gallows gallows = new Gallows();
+        switch (wrongAttempts) {
+            case 6:
+                gallows.sixErrorsLeft();
+                break;
+            case 5:
+                gallows.fiveErrorsLeft();
+                break;
+            case 4:
+                gallows.foreErrorsLeft();
+                break;
+            case 3:
+                gallows.threeErrorsLeft();
+                break;
+            case 2:
+                gallows.twoErrorsLeft();
+                break;
+            case 1:
+                gallows.oneErrorsLeft();
+                break;
+            case 0:
+                gallows.zeroErrorsLeft();
+                break;
+            default:
+                break;
         }
-        return stringBuilder.toString();
     }
 }
